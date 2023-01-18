16f73a3a6ad35b86bb1684cfe0ff652a40f15be8
Sasha Maslak <95244349+SashaMaslak@users.noreply.github.com>
2023-01-18 15:38:31 +0200

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,28 @@
+import { useMedia } from 'react-use';
 import { GlobalStyle } from './GlobalStyle';
-// import { Box } from './Box';
+import { Box } from './Box';
 import { Recipe } from './Recipe/Recipe';
 import recipes from '../recepies.json';
 
 export const App = () => {
+  const isDesktop = useMedia('(min-width: 768px)');
   return (
-    <div>
-      {recipes.map((recipe, idx) => (
-        <Recipe key={idx} recipe={recipe} />
-      ))}
-      <GlobalStyle />
-    </div>
+    <>
+      <Box
+        as="header"
+        display="flex"
+        alignItems="center"
+        justifyContent="space-between"
+      >
+        <div>Logo</div>
+        {isDesktop ? <div>Desktop nav</div> : <div>Mobile nav</div>}
+      </Box>
+      <Box>
+        {recipes.map((recipe, idx) => (
+          <Recipe key={idx} recipe={recipe} />
+        ))}
+        <GlobalStyle />
+      </Box>
+    </>
   );
 };
